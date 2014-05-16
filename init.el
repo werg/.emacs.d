c7ea5070c0c4004cc43078fbe3a771eea4f88bb4
Gabriel Pickard <gpickard@uos.de>
2014-05-16 12:08:38 -0700

    alter height

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
   (tool-bar-mode -1))
 
 ;; make transparent if the window manager supports it
-(add-to-list 'default-frame-alist '(alpha 85 75))
+(add-to-list 'default-frame-alist '(alpha 95 85))
 
 (setq frame-title-format '("%f"))
 
@@ -419,7 +419,7 @@ Leave one space or none, according to the context."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 140)))))
+ '(default ((t (:height 100)))))
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
